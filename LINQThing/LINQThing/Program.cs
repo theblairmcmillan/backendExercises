019bac10a04be94b83c520efe75dbba4b69f0de9
theblairmcmillan <theblairmcmillan@gmail.com>
2016-05-03 09:46:19 -0500

    quiz 20 tests not passing

---
+++ LINQThing/LINQThing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQlivecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "wonderful", "LINQ", "beautiful", "world" };
+
+            // Get only short words
+            var shortWords = from word in words where word.Length <= 5 select word;
+
+            // Print each word out
+
+            foreach (var word in shortWords)
+
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SharpShapes/SharpShapes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpShapes
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var list = new List<KeyValuePair<int, string>>();
+            list.Add(new KeyValuePair<int, string>(1, "square"));
+            list.Add(new KeyValuePair<int, string>(2, "circle"));
+            list.Add(new KeyValuePair<int, string>(3, "rectangle"));
+
+            foreach (var List in list)
+            {
+                Console.WriteLine(List);
+               
+            }
+            Console.ReadLine();
+        }
+      
+    }
+    
+}
